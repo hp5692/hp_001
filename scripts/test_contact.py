091a70565a1f48f8eca69fe51c76484945684f12
hp5692 <hanpeng5692@163.com>
2018-11-12 15:24:04 +0800

    test

---
+++ scripts/test_contact.py
@@ -0,0 +1,15 @@
+import pytest
+
+class TestContact:
+
+    def test_1(self):
+        print("1")
+        assert 1
+
+    def test_2(self):
+        print("2")
+        assert 1
+
+    def test_3(self):
+        print("3")
+        assert 0
